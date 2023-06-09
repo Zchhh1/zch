02b44ea44ddd17092a5e201ff05214eeecfadd4d
Zchhh1 <94824053+Zchhh1@users.noreply.github.com>
2023-06-09 10:25:11 +0800

    Add files via upload

---
+++ hello_work.py
@@ -0,0 +1,19 @@
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+zch = "Hello zch, would you liketo learn some Python today"
+print (zch)
+
+zch= "xiao chai mei mei"
+print (zch.title())
+print (zch.upper())
+print (zch.lower())
+
+
+print ("Alber Einstein oncesaid, "  "Apersonwho never madea mistake never tried anything new.")
+
+famous_name= "Alber Einstein oncesaid,"
+
+print (famous_name     +   "Apersonwho never medea mistake never tried anything new.")
+print (famous_name.title()     +   "Apersonwho never medea mistake never tried anything new.")
+
